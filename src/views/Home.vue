887621924b1a50c4acfb6aaa3d8a6fb134af89f9
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-25 17:30:59 -0400

    adds axios, index, and create actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <!-- {{ movies }} -->
+    <h3>Add a Movie</h3>
+    <p>title: <input type="text" v-model="newMovieTitle"></p>
+    <p>year: <input type="text" v-model="newMovieYear"></p>
+    <p>plot: <input type="text" v-model="newMoviePlot"></p>
+    <p>director: <input type="text" v-model="newMovieDirector"></p>
+    <p>english: <input type="text" v-model="newMovieEnglish"></p>
+    <button v-on:click="createMovie"> ADD</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      {{ movie.title }}
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to the dark side",
+      movies: [],
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: "",
+      newMovieEnglish: "",
+    };
+  },
+  created: function () {
+    console.log("in created");
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      console.log("in moviesIndex");
+      axios.get("http://localhost:3000/api/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      console.log("in create movie");
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector,
+        english: this.newMovieEnglish,
+      };
+      axios.post("http://localhost:3000/api/movies", params).then((response) => {
+        console.log(response.data);
+        this.movies.push(response.data);
+      });
+    },
+  },
+};
 </script>
